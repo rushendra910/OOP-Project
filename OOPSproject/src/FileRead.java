3d75d73d7d234ff1a358ba19306b6c75bc9dd172
AmalSN <amalkrish12@gmail.com>
2021-12-01 17:58:36 +0530

    Correct Fields

--- OOPSproject/src/FileRead.java
+++ OOPSproject/src/FileRead.java
@@ -7,38 +7,40 @@ Need to create table class with proper columns
 package OOPSproject.src;
 
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 class Table{
     //Column Names(We can change these as required)
     private int id;
     private String name;
-    private int salary;
+    private String severity;
+    private Boolean recovered;
+    private Boolean vaccinated;
 
     //Table Constructor
-    public Table(int id,String name,int salary){
+    public Table(int id,String name,String severity,Boolean recovered,Boolean vaccinated){
         this.id=id;
         this.name=name;
-        this.salary=salary;
+        this.severity=severity;
+        this.recovered=recovered;
+        this.vaccinated=vaccinated;
     }
 
     //toString() utility function
     @Override
     public String toString(){
-        String str="Id: "+this.id+"\nName: "+this.name+"\nSalary: "+this.salary;
+        String str="Id: "+this.id+"\nName: "+this.name+"\nSeverity: "+this.severity+"\nRecovered: "+this.recovered+"\nVaccinated: "+this.vaccinated;
         return str;
     }
 
     //Method to convert any CSV file to a Table array
-    public static Table[] CSVToTable(String filePath){
-        Table[] t=new Table[100];  //Increase the size of the table array to accomodate more data
-        int pos=0;
+    public List<Table> CSVToTable(String filePath){
+        List<Table> t=new ArrayList<>();
         try{
             Scanner sin=new Scanner(new BufferedReader(new FileReader(filePath)));
             while(sin.hasNext()){
-                String str=sin.nextLine();
-                String[] tokens=str.split(",");
-                t[pos++]=new Table(Integer.parseInt(tokens[0]),tokens[1],Integer.parseInt(tokens[2]));
+                String[] tokens=sin.nextLine().split(",");
+                t.add(new Table(Integer.parseInt(tokens[0]),tokens[1],tokens[2],Boolean.parseBoolean(tokens[3]),Boolean.parseBoolean(tokens[4])));
             }
             sin.close();
         }
